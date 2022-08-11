8df2912b91e44c19c9c668c76be8ef4b52ac2c74
Anais Augu <augu.anais@gmail.com>
2022-08-11 15:05:51 +0200

    Base config
    Images, variables, routes, controller pages, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+	root to: 'pages#home'
+	get 'about', to: 'pages#about'
+	get 'contact', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
